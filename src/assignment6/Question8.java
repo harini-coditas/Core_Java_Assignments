51f52fbcc4c6ceaba2598e5ef667dce303738fbf
harini-coditas <harini.bathini@coditas.com>
2023-02-07 17:05:33 +0530

    Assignment 6

---
+++ src/assignment6/Question8.java
@@ -0,0 +1,53 @@
+package assignment6;
+
+class Pattern1A{
+    public void print1A(){
+        for( int i=0 ; i < 5 ; i++){
+            for(int j=0 ; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+class Pattern1B{
+    public void print1B(){
+        char charArray[] = {'A','B','C','D','E','F'};
+
+        for( int i=5 ; i >= 0 ; i--){
+            for(int j=0 ; j <= i ; j++){
+                System.out.print(charArray[j]+" ");
+            }
+            System.out.println();
+        }
+        for( int i=0 ; i < 6 ; i++){
+            for(int j=0 ; j <= i ; j++){
+                System.out.print(charArray[j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+class Pattern2A{
+    public void print2A(){
+        for( int i=9 ; i >= 1 ; i--){
+
+            for(int j=1 ; j <= i ; j++) {
+
+                System.out.print("*");
+            }
+            i=i-1;
+            System.out.println();
+        }
+    }
+}
+public class Question8 {
+    public static void main(String[] args) {
+        Pattern1A pattern1 = new Pattern1A();
+        pattern1.print1A();
+        Pattern1B pattern2 = new Pattern1B();
+        pattern2.print1B();
+        Pattern2A pattern3 = new Pattern2A();
+        pattern3.print2A();
+    }
+}
