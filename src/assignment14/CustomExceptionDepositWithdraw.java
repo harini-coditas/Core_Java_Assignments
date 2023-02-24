b3081545ea341a5864f9fea9a18f56bedb725c4d
harini-coditas <harini.bathini@coditas.com>
2023-02-24 14:02:44 +0530

    Assignment 14 - Custom Exception

--- src/assignment14/CustomExceptionDepositWithdraw.java
+++ src/assignment14/CustomExceptionDepositWithdraw.java
@@ -8,12 +8,12 @@ public class CustomExceptionDepositWithdraw {
         System.out.println("Please enter '1' for DEPOSIT and enter '2' for WITHDRAWAL");
         int value = sc.nextInt();
         AmountExceptionChecker amountExceptionChecker = new AmountExceptionChecker();
-        if(value==1) {
+        if(value==1){
             System.out.println("Please enter DEPOSIT AMOUNT");
             int num1 = sc.nextInt();
             amountExceptionChecker.deposit(num1);
         }
-        if(value==2) {
+        if(value==2){
             System.out.println("Please enter the withdrawal amount");
             int num2 = sc.nextInt();
             amountExceptionChecker.withdrawal(num2);
