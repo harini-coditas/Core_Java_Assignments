ca057b1c6663422a9e4354fcf2c1d73887cf7fee
harini-coditas <harini.bathini@coditas.com>
2023-03-09 15:22:14 +0530

    evaluation 5

---
+++ src/evaluation5/User.java
@@ -0,0 +1,47 @@
+package evaluation5;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+
+public class User {
+    Scanner sc = new Scanner(System.in);
+    Random random = new Random();
+  //  long mobileNo = random.nextLong() % 10000000000L;
+  HashSet<Integer> mobNoSet = new HashSet<>();
+    public int validity;
+    public int amount;
+    public int iniBal;
+    public void newConnection(){
+        System.out.println("--- New Connection --");
+        System.out.println("Please enter your details--");
+        System.out.println("Enter name ");
+        String name = sc.nextLine();
+        System.out.println("Enter email ");
+        String email = sc.nextLine();
+        System.out.println("Enter address ");
+        String address = sc.nextLine();
+        System.out.println("Enter aadhaarNo ");
+        String aadhaarNo = sc.nextLine();
+        System.out.println("Enter SimType: prepaid/postpaid ");
+        String simType = sc.nextLine();
+        System.out.println("Enter initial balance ");
+        iniBal = sc.nextInt();
+        validity = 30;
+
+        int mobileNo = random.nextInt(900000000) + 1000000000;
+        System.out.println("Your mobile no is: "+mobileNo);
+
+
+//        HashSet<Long> mobNoSet = new HashSet<>();
+        mobNoSet.add(mobileNo);
+
+
+    }
+    public void viewAllUsers(){
+        for(Integer i : mobNoSet){
+            System.out.println("Mobile numbers of users are: "+i);
+        }
+
+    }
+}
